024467c31dd8aed3840555e4360cb2898e692110
itslisaram <lisaramc@gmail.com>
2025-04-13 11:54:49 +0200

    Element creation

---
+++ src/app/components/coin-detail/coin-detail.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-coin-detail',
+  imports: [],
+  templateUrl: './coin-detail.component.html',
+  styleUrl: './coin-detail.component.css'
+})
+export class CoinDetailComponent {
+
+}
---
+++ src/app/components/crypto-table/crypto-table.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crypto-table',
+  imports: [],
+  templateUrl: './crypto-table.component.html',
+  styleUrl: './crypto-table.component.css'
+})
+export class CryptoTableComponent {
+
+}
