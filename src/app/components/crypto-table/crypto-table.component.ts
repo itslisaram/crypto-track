167c4125e9fe8323b347fd8eb0eeb8da961fc6c5
itslisaram <lisaramc@gmail.com>
2025-04-18 15:01:34 +0200

    Table using Coingecko API - Real Data

--- src/app/components/crypto-table/crypto-table.component.ts
+++ src/app/components/crypto-table/crypto-table.component.ts
@@ -1,13 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CryptoService } from '../../services/crypto.service';
 
 @Component({
   selector: 'app-crypto-table',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './crypto-table.component.html',
   styleUrls: ['./crypto-table.component.css']
 })
+export class CryptoTableComponent implements OnInit {
+  cryptos: any[] = [];
 
-export class CryptoTableComponent {
+  constructor(private cryptoService: CryptoService) {}
 
+  ngOnInit(): void {
+    this.cryptoService.getTopCryptos().subscribe((data: any) => {
+      this.cryptos = data;
+    });
+  }
 }
--- src/app/services/crypto.service.ts
+++ src/app/services/crypto.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CryptoService {
+  private apiUrl = 'https://api.coingecko.com/api/v3/coins/markets';
+  private params = '?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false';
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getTopCryptos(): Observable<any> {
+    return this.http.get<any>(`${this.apiUrl}${this.params}`);
+  }
 }
